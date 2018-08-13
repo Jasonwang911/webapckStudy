8d7e886482529300ca2f13841d46579afab2e982
Jasonwang911 <396689144@qq.com>
2018-08-13 14:38:01 +0800

    搭建简单环境

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {test: /\.js$/, use: 'babel-loader', exclude: /node_modules/},
             {test: /\.css$/, use: ['style-loader', 'css-loader']},
-            {text: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']}
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']}
         ]
     },
     plugins: [
